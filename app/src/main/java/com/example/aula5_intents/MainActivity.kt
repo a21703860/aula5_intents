cd87c85020183ffcbdff5e404b0e0d56b22ec079
AfonsoMmsc <afonsonogueirammsc@gmail.com>
2023-03-25 02:26:42 +0000

    intents

--- app/src/main/java/com/example/aula5_intents/MainActivity.kt
+++ app/src/main/java/com/example/aula5_intents/MainActivity.kt
@@ -1,11 +1,40 @@
 package com.example.aula5_intents
 
+import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aula5_intents.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+     lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+
+    override fun onStart() {
+        super.onStart()
+        val name = intent.getStringExtra(EXTRA_NAME)
+        name?.let { binding.textWelcolme.text = it }
+
+        binding.buttonGo.setOnClickListener {
+            startActivity(Intent(this, MainActivity2::class.java))
+            finish()
+        }
+
+        binding.buttonPhone.setOnClickListener {
+            startActivity(Intent(Intent.ACTION_DIAL))
+        }
+
+        binding.buttonPhoneNumber.setOnClickListener {
+            val intent = Intent(Intent.ACTION_DIAL)
+            intent.data = Uri.parse("tel:0123456789")
+            startActivity(intent)
+        }
+
     }
 }
+
--- app/src/main/java/com/example/aula5_intents/MainActivity2.kt
+++ app/src/main/java/com/example/aula5_intents/MainActivity2.kt
@@ -1,11 +1,32 @@
 package com.example.aula5_intents
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.aula5_intents.databinding.ActivityMain2Binding
+import com.example.aula5_intents.databinding.ActivityMainBinding
+
+const val EXTRA_NAME = "pt.ulusofona.ecati.deisi.cm.calculadora.NAME"
 
 class MainActivity2 : AppCompatActivity() {
+    private lateinit var binding: ActivityMain2Binding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main2)
+        binding = ActivityMain2Binding.inflate(layoutInflater)
+        setContentView(binding.root)
     }
+
+    override fun onStart() {
+        super.onStart()
+        binding.buttonBack.setOnClickListener {
+            val intent = Intent(this, MainActivity::class.java)
+            intent.apply {
+                putExtra(EXTRA_NAME, binding.editWelcolme.text.toString())
+            }
+            startActivity(intent)
+            finish()
+        }
+    }
+
 }
